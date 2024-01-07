76936935b18159b23fdd9adce58d543d12addafc
dkile <dkile532@naver.com>
2024-01-07 12:59:18 +0900

    Feat: 카드 썸네일 컴포넌트 next image로 변경

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,38 +1,44 @@
+import Image from "next/image";
 import {
-	ComponentPropsWithRef,
-	ComponentPropsWithoutRef,
-	ReactNode,
+  ComponentPropsWithRef,
+  ComponentPropsWithoutRef,
+  HTMLAttributes,
+  ReactNode,
 } from "react";
 
 function Card({ children, ...props }: ComponentPropsWithRef<"div">) {
-	return <div {...props}>{children}</div>;
+  return <div {...props}>{children}</div>;
 }
 
-function Thumbnail({ src, alt, ...props }: ComponentPropsWithoutRef<"img">) {
-	return <img {...props} src={src} alt={alt} />;
+interface ThumbnailProps extends HTMLAttributes<HTMLImageElement> {
+  src: string;
+  alt: string;
+}
+function Thumbnail({ src, alt, ...props }: ThumbnailProps) {
+  return <Image {...props} src={src} alt={alt} />;
 }
 
 interface TitleProps
-	extends ComponentPropsWithoutRef<"h2" | "h3" | "h4" | "h5" | "h6"> {
-	label: {
-		content: ReactNode;
-		headType: "h2" | "h3" | "h4" | "h5" | "h6";
-	};
+  extends ComponentPropsWithoutRef<"h2" | "h3" | "h4" | "h5" | "h6"> {
+  label: {
+    content: ReactNode;
+    headType: "h2" | "h3" | "h4" | "h5" | "h6";
+  };
 }
 function Title({ label, ...props }: TitleProps) {
-	const { content, headType: H } = label;
-	return <H {...props}>{content}</H>;
+  const { content, headType: H } = label;
+  return <H {...props}>{content}</H>;
 }
 
 interface DescriptionProps extends ComponentPropsWithoutRef<"p"> {
-	description: ReactNode;
+  description: ReactNode;
 }
 function Description({ description, ...props }: DescriptionProps) {
-	return <p {...props}>{description}</p>;
+  return <p {...props}>{description}</p>;
 }
 
 export default Object.assign(Card, {
-	Thumbnail,
-	Title,
-	Description,
+  Thumbnail,
+  Title,
+  Description,
 });
