47c979caa73aa95d642021d571ee59f81b4faf08
dkile <dkile532@naver.com>
2024-01-11 18:08:49 +0900

    Feat: LinkInfo 타입에 nullable 필드 추가

--- src/types/folder.ts
+++ src/types/folder.ts
@@ -1,23 +1,23 @@
 export type Folder = {
-	id: number;
-	created_at: string;
-	name: string;
-	user_id: number;
-	favorite: boolean;
-	link: {
-		count: number;
-	};
+  id: number;
+  created_at: string;
+  name: string;
+  user_id: number;
+  favorite: boolean;
+  link: {
+    count: number;
+  };
 };
 
 export type CurrentFolder = Pick<Folder, "id" | "name">;
 
 export type LinkInfo = {
-	id: number;
-	created_at: string;
-	updated_at: string | null;
-	url: string;
-	title: string | null;
-	description: string | null;
-	image_source: string | null;
-	folder_id: number | null;
+  id: number;
+  created_at: string;
+  updated_at: string | null;
+  url: string;
+  title: string | null;
+  description: string | null;
+  image_source: string | null;
+  folder_id: number | null;
 };
